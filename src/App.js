17391d0df58ee4836b11a7fd454e3c5ad4c7cccf
wallace machado <wallacemachado@ntopus.com>
2022-08-25 19:43:24 -0300

    adicionando novas atividades

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 
+let initalAtividades = [
+  {
+    id: 1,
+    descricao: "Primeira Atividade"
+  },
+  {
+    id: 2,
+    descricao: "Segunda Atividade"
+  }
+]
+
 function App() {
+  const [atividades, setAtividades]= useState(initalAtividades)
+
+
+  function addAtividade(e) {
+    e.preventDefault();
+
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
+    }
+    setAtividades([...atividades, {...atividade}]);
+  }
+  
+
+  // se fosse usar html puro não funciona pq apagina não atualiza sozinha, por isso vamos usar o hooks
+  // function addAtividade(e) {
+  //   e.preventDefault();
+
+  //   const atividade = {
+  //     id: document.getElementById('id').value,
+  //     descricao: document.getElementById('descricao').value,
+  //   }
+  //   atividades.push(atividade);
+  //   console.log(atividades)
+  // }
   return (
+    <>
+    <form>
+      <input id='id' type='text' placeholder='id'/>
+      <input id='descricao' type='text' placeholder='descricao'/>
+      <button onClick={addAtividade}>+ Atividade</button>
+    </form>
     <div className="mt-3">
        <ul className='list-group'>
-         <li className='list-group-item'>Primeira Atividade</li>
-         <li className='list-group-item'>Segunda Atividade</li>
-         <li className='list-group-item'>Terceira Atividade</li>
-         <li className='list-group-item'>Quarta Atividade</li>
+        {atividades.map(ativ => (
+          <li key={ativ.id} className='list-group-item'>{ativ.id} - {ativ.descricao}</li>  
+        ))}                
        </ul>    
     </div>
+    </>
   );
 }
 
